a0f311753402a423a0eab30155980e8a6dc56377
aHuang <adamhut@gmail.com>
2021-06-13 20:18:20 +0800

    wip ep 35

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/', function () {
         'categories' => Category::all(),
 
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}',function(Post $post){
     //Find a post by its slug  and pass it to a view call "post"
@@ -68,7 +68,7 @@ Route::get('categories/{category:slug}',function(Category $category){
         'currentCategory' => $category,
         'categories' => Category::all(),
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
 
