2fef0ce4ccee7a375873607c97c7f19138818c72
adamhut <adamhut@gmail.com>
2021-06-18 05:23:40 -0700

    wip

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -22,6 +22,7 @@ class RegisterController extends Controller
             'email'     => ['required','email','max:255'],
             'password'  => ['required','max:255','min:7'],
         ]);
+        $attributes['password'] = bcrypt($attributes['password']);
 
         User::create($attributes);
 
